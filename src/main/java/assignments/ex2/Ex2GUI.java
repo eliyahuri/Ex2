6ed05cc516cc66fa47bcb4daa8336b5b2bfdd643
eliyahuri <tkhvu1236@gmail.com>
2025-01-13 20:04:18 +0200

    Fix display format in Ex2GUI to improve clarity of cell information

--- src/main/java/assignments/ex2/Ex2GUI.java
+++ src/main/java/assignments/ex2/Ex2GUI.java
@@ -145,7 +145,7 @@ public class Ex2GUI {
 		if (table.isIn(xx, yy)) {
 			Cell cc = table.get(xx, yy);
 			cord = new CellEntry(xx, yy);
-			String ww = cord + ": " + cc.toString() + " : ";
+			String ww = cord + " : ";
 			StdDrawEx2.text(Ex2Utils.GUI_X_START, Ex2Utils.MAX_X - 1, ww);
 			StdDrawEx2.show();
 			if (Ex2Utils.Debug) {
